7c82b988f5f066de351592d3a5ce2d2ff548dd88
ismail kahraman <ismailokahraman@gmail.com>
2024-10-02 22:22:08 +0300

    Add AccordionContent component for managing the content of each accordion item

---
+++ src/components/Accordion/AccordionContent.jsx
@@ -0,0 +1,20 @@
+import { useAccordionContext } from "./Accordion";
+import { useAccordionItemContext } from "./AccordionItem";
+
+function AccordionContent({ className, children }) {
+  const { openItemId } = useAccordionContext();
+  const id = useAccordionItemContext();
+
+  const isOpen = openItemId === id;
+  return (
+    <div
+      className={
+        isOpen ? `${className ?? ""} open` : `${className ?? ""} close`
+      }
+    >
+      {children}
+    </div>
+  );
+}
+
+export default AccordionContent;
