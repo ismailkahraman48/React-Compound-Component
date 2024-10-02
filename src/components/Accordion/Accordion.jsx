f134ecb0595a361342376eb34e53b686d812e0e1
ismail kahraman <ismailokahraman@gmail.com>
2024-10-02 22:22:03 +0300

    Refactor Accordion component to use context for managing state

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { createContext, useContext, useState } from "react";
+import "./Accordion.css";
+import AccordionItem from "./AccordionItem";
+import AccordionTitle from "./AccordionTitle";
+import AccordionContent from "./AccordionContent";
+const AccordionContext = createContext();
 
-function Accordion({ children, className }) {
-  return <ul className={className}>{children}</ul>;
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+
+  //! Throw an error if the component is not used within Accordion
+  if (!ctx) {
+    throw new Error("AccordionItem must be used within Accordion");
+  }
+
+  return ctx;
 }
 
-export default Accordion;
+export default function Accordion({ children, className }) {
+  const [openItemId, setOpenItemId] = useState();
+
+  function toggleItem(id) {
+    console.log(id);
+    setOpenItemId((prevId) => (prevId === id ? null : id));
+  }
+
+  const contextValue = {
+    openItemId,
+    toggleItem,
+  };
+
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
+}
+
+Accordion.item = AccordionItem;
+Accordion.title = AccordionTitle;
+Accordion.Content = AccordionContent;
